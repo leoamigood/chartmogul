b2021732f33acbf1a4631f01e6bbceb26311e2ee
Leo Amigood <leo@amigood.com>
2022-11-21 15:03:20 +0100

    Basic db and sidekiq setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 # frozen_string_literal: true
 
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
+  mount Sidekiq::Web, at: '/sidekiq'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
